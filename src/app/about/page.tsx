8e769eeaf66bc8f42c51588a73b502c42c4f3551
ArmanKamal2 <Arman.Kamal@employbridge.com>
2024-03-29 07:37:12 -0500

    About page added

---
+++ src/app/about/page.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className='max-w-6xl mx-auto p-3  space-y-4'>
+        <h1 className='text-2xl font-medium text-purple-500'>
+            About
+        </h1>
+        <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+             Ab dolore debitis odio alias deserunt iste officia quod pariatur nobis
+            aperiam, natus architecto dolorum, ratione fugiat amet quis minima quas 
+            at excepturi provident inventore nesciunt dolores! In debitis porro esse
+            ab veritatis sequi, sapiente totam minima officia mollitia harum temporibus dolor,
+        </p>
+        <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+             Ab dolore debitis odio alias deserunt iste officia quod pariatur nobis
+            aperiam, natus architecto dolorum, ratione fugiat amet quis minima quas 
+            at excepturi provident inventore nesciunt dolores! In debitis porro esse
+            ab veritatis sequi, sapiente totam minima officia mollitia harum temporibus dolor,
+            dolorem suscipit laboriosam ut, neque sunt veniam. Sed distinctio cupiditate
+         
+        </p>
+        <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+             Ab dolore debitis odio alias deserunt iste officia quod pariatur nobis
+            aperiam, natus architecto dolorum, ratione fugiat amet quis minima quas 
+            at excepturi provident inventore nesciunt dolores! In debitis porro esse
+            ab veritatis sequi, sapiente totam minima officia mollitia harum temporibus dolor,
+            dolorem suscipit laboriosam ut, neque sunt veniam. Sed distinctio cupiditate
+            incidunt molestias iure tempore dolore dolorum repellat optio. 
+        </p>
+    </div>
+   
+  )
+}
